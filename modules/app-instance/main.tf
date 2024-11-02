9adce0e1d4179c22a807436200a808044ce7842e
kundathilnithi <kundathil.nithi@gmail.com>
2024-11-02 15:34:54 +0530

    backend config

--- modules/app-instance/main.tf
+++ modules/app-instance/main.tf
@@ -2,10 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
-}
+
 resource "aws_instance" "app_server" {
   count = var.server_count
   ami   = "ami-0866a3c8686eaeeba"
